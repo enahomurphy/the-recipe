fb9885db013486273c99543bf9f9cafb7ccb84e7
andela-emurphy <murphy.enaho@andela.com>
2017-05-26 04:55:21 +0100

    add authentication middleware

--- authentication/login.go
+++ authentication/login.go
@@ -1,12 +1,15 @@
 package authentication
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"recipe/helpers"
 	"recipe/jwt"
 	"recipe/models"
+	"strings"
 	"time"
 )
 
@@ -50,4 +53,26 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	token := jwt.Encode(payload, secret)
 
 	helpers.StatusOk(w, Token{Token: token})
+}
+
+// AuthMiddleware routes
+func AuthMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// w.Header().Set("Content-Type", "application/json")
+		auth := r.Header.Get("Authorization")
+		token := strings.Split(auth, " ")
+		if len(token) != 2 {
+			helpers.Unauthorized(w, "Authentication failed: Invalid token")
+			return
+		}
+		payload, err := jwt.Decode(token[1], "secret")
+		fmt.Println(payload)
+		if err != nil {
+			helpers.Unauthorized(w, err.Error())
+			return
+		}
+		ctx := context.WithValue(r.Context(), "user", payload)
+		h.ServeHTTP(w, r.WithContext(ctx))
+		// h.ServeHTTP(w, r)
+	})
 }
